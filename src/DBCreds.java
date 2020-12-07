11c9186104ef8168ec1cbf65c29454cb7397139a
Jayesh Padhiar <jayeshpadhiar20@gmail.com>
2020-12-07 11:56:05 +0530

    Added Screenshots for README.md

--- src/DBCreds.java
+++ src/DBCreds.java
@@ -2,10 +2,13 @@ public class DBCreds {
     public String url, dbName, dbUrl, user, pass;
 
     public DBCreds(){
+
+        ///*
         this.url = "jdbc:mysql://localhost:3306/";
         this.dbName = "freelance";
-        this.dbUrl = url+dbName+"?autoReconnect=true";
-        this.user = "root";
-        this.pass = "password";
+        this.dbUrl = "jdbc:mysql://remotemysql.com:3306/"+dbName+"?autoReconnect=true";
+        this.user = "6RXBPbWeHI";
+        this.pass = "f5scOsynl4";
+        //*/
     }
 }
